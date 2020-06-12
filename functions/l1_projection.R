01b4d8a177f007db060c7d4e83ae7615d3891bc5
Corey Yanofsky <corey.yanofsky@gmail.com>
2020-06-11 23:57:32 -0400

    some functions that might be useful

---
+++ functions/l1_projection.R
@@ -0,0 +1,32 @@
+lagrange_multiplier_for_projection_onto_l1_ball <- function(x, r) {
+  # assumes that all x are non-negative
+  if(sum(x) <= r) return (0)
+  y <- sx <- sort(x, decreasing = TRUE)
+  y <- (cumsum(y) - r) / seq_along(y)
+  y[y < 0] <- 0
+  c <- max(which(sx > y))
+  y[c]
+}
+
+l1_projection <- function(x, r) {
+  ss <- sign(x)
+  x <- abs(x)
+  proj_x <- x - lagrange_multiplier_for_projection_onto_l1_ball(x, r)
+  proj_x[proj_x < 0] <- 0
+  proj_x*ss
+}
+
+smooth_ramp <- function(x) {
+  nice_curve <- function(x) 25*(x^3) - 62.5*(x^4)
+  ifelse(
+    x < -0.1, 0, 
+  ifelse(
+    x > 0.1, x,
+  ifelse(x < 0, nice_curve(0.1 + x), 
+    x + nice_curve(0.1 - x)
+  )))
+}
+
+smooth_threshold_fun <- function(x, threshold) {
+  smooth_ramp(x - threshold) - smooth_ramp(-threshold - x)
+}
